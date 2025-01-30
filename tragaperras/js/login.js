3902a0a242692aee6fbacb0785dd303a925bd5c5
jesuruiz-birt <jesuruiz@birt.eus>
2025-01-30 19:53:33 +0100

    Entrega

--- tragaperras/js/login.js
+++ tragaperras/js/login.js
@@ -1,4 +1,4 @@
-let usersJSONPath = '';
+let usersJSONPath = 'data/users.json';
 
 function cargarJSONEnLocalStorage() {
     fetch(usersJSONPath)
@@ -19,9 +19,34 @@ function cargarJSONEnLocalStorage() {
 
 // Función que valida el usuario y la contraseña
 function validarCredenciales() {
+    let usuario = document.getElementById("username").textContent;
+    let contrasenia = document.getElementById("password").textContent;
+    console.log (usuario);
+    console.log (contrasenia);
 
+    let usersJSON = []
+    let users = localStorage.getItem("usuarios");
+    usersJSON = JSON.parse(users);
+    
+    //console.log (localStorage.getItem("usuarios"));
+
+    console.log (usersJSON);
+/*
+    array.forEach(_usersJSON => {
+        if (usersJSON.username == localStorage.username) {
+            console.log ("El usuario es correcto");
+        } else if(sersJSON.username == localStorage.username) {
+            
+        }
+    });
+*/
+    let i;
+    for (i = 0 ; i < usersJSON.length; i++) {
+        console.log(usersJSON);
+    }
 }
 
+cargarJSONEnLocalStorage();
 
-
+document.getElementById("login-button").onclick = validarCredenciales();
 
